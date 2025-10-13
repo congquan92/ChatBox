fb8025e7ade9dff0fd76aee26e9a8ff0d866b434
congquan92 <145172432+congquan92@users.noreply.github.com>
2025-10-13 17:22:58 +0700

    tmp 4

--- client/src/api/conversations.ts
+++ client/src/api/conversations.ts
@@ -14,6 +14,20 @@ export interface Conversation {
     lastMessageTime: string;
     lastMessageSender: string;
 }
+
+export interface Message {
+    id: number;
+    conversationId: number;
+    senderId: number;
+    content: string;
+    contentType: "text" | "image" | "video";
+    createdAt: string;
+    editedAt: string | null;
+    username: string;
+    displayName: string;
+    avatarUrl: string;
+}
+
 export async function getUserConversations() {
     try {
         const response = await fetch(`${import.meta.env.VITE_API_URL}/conversations`, {
@@ -30,6 +44,26 @@ export async function getUserConversations() {
         return []; // Trả về mảng rỗng nếu không thành công
     } catch (error) {
         console.error("Error fetching conversations:", error);
+        return []; // Trả về mảng rỗng nếu có lỗi
+    }
+}
+
+export async function getConversationById(conversationId: number) {
+    try {
+        const response = await fetch(`${import.meta.env.VITE_API_URL}/conversations/${conversationId}`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${localStorage.getItem("token") || ""}`,
+            },
+        });
+        if (response.ok) {
+            const data = await response.json();
+            return data.conversation as Conversation[];
+        }
+        return []; // Trả về mảng rỗng nếu không thành công
+    } catch (error) {
+        console.error("Error fetching conversation by ID:", error);
         return []; // Trả về mảng rỗng nếu có lỗi
     }
 }
